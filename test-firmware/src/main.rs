53f9c193d318e482b6cc3f696681da464b1e8438
Sjoerd Simons <sjoerd@collabora.com>
2025-02-22 09:57:34 +0100

    Enable display

--- test-firmware/src/main.rs
+++ test-firmware/src/main.rs
@@ -5,13 +5,53 @@ use defmt::info;
 // Needed to defmt rtt channel
 use defmt_rtt as _;
 use embassy_executor::Spawner;
+use embassy_stm32::{bind_interrupts, i2c, peripherals, time::Hertz};
+use embedded_graphics::mono_font::ascii::FONT_6X12;
+use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::*;
+use embedded_graphics::text::{Baseline, Text};
 // Sets the panic handler
 use panic_probe as _;
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306Async};
+
+bind_interrupts!( struct Irqs {
+    I2C1 => i2c::EventInterruptHandler<peripherals::I2C1>,
+            i2c::ErrorInterruptHandler<peripherals::I2C1>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let config = embassy_stm32::Config::default();
-    let _p = embassy_stm32::init(config);
+    let p = embassy_stm32::init(config);
 
     info!("Oh hi!");
+
+    // Display i2c
+    let i2c = embassy_stm32::i2c::I2c::new(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Irqs,
+        p.DMA1_CH6,
+        p.DMA1_CH7,
+        Hertz::khz(400),
+        Default::default(),
+    );
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate180)
+        .into_buffered_graphics_mode();
+
+    display.init().await.unwrap();
+
+    let style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X12)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Oh, hi", Point::new(4, 8), style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    display.flush().await.unwrap();
 }
